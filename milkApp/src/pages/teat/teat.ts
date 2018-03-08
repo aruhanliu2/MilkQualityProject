b6a15a6900e6747cdcc6904f824236037c60f1a8
RobinChenRichmond <guanyu.chen@richmond.edu>
2018-03-07 23:48:21 -0500

    Teat Cleanliness form finished
    finished the prototype of teat cleanliness form

--- milkApp/src/pages/teat/teat.ts
+++ milkApp/src/pages/teat/teat.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 /**
  * Generated class for the TeatPage page.
@@ -14,8 +15,51 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'teat.html',
 })
 export class TeatPage {
+  public clean: number = 0;
+  public deepPresent: number = 0;
+  public smallDirt: number = 0;
+  public largeDirt: number = 0;
+  constructor(public alerCtrl: AlertController) {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+  tapDecrease(e,param:number){
+    if(param==1){
+      this.clean = Math.max(this.clean-1,0)
+    } else if(param==2){
+      this.deepPresent = Math.max(this.deepPresent-1,0)
+    } else if(param==3){
+      this.smallDirt = Math.max(this.smallDirt-1,0)
+    } else if(param==4){
+      this.largeDirt = Math.max(this.largeDirt-1,0)
+    }
+
+  }
+  tapIncrease(e,param:number) {
+    if(param==1){
+      this.clean++
+    } else if(param==2){
+      this.deepPresent++
+    } else if(param==3){
+      this.smallDirt++
+    } else if(param==4){
+      this.largeDirt++
+    }
+  }
+  saveData() {
+    let alert = this.alerCtrl.create({
+      title: 'Saved!',
+      message: 'Data have been saved locally!',
+      buttons: ['Ok']
+    });
+    alert.present()
+  }
+  submitData() {
+    let alert = this.alerCtrl.create({
+      title: 'Submitted!',
+      message: 'Data have been submitted!',
+      buttons: ['Ok']
+    });
+    alert.present()
   }
 
   ionViewDidLoad() {
