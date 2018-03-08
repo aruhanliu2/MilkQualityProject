3c47b03d301677c05e86088d852d093a4ba20b69
Youming Luo <yl2397@cornell.edu>
2018-03-08 15:06:35 -0500

    add unit alignment
    add unit alignment

--- milkApp/src/pages/alignment/alignment.ts
+++ milkApp/src/pages/alignment/alignment.ts
@@ -14,6 +14,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'alignment.html',
 })
 export class AlignmentPage {
+  myDate: String = new Date().toISOString();
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
