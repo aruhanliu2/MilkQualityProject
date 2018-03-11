d439960fc723d9f9c292ef5bfa8b5156a1ef75a1
RobinChenRichmond <guanyu.chen@richmond.edu>
2018-03-11 16:03:00 -0400

    finish postmilk form

--- milkApp/src/pages/postmilk/postmilk.ts
+++ milkApp/src/pages/postmilk/postmilk.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Validators, FormBuilder, FormGroup } from '@angular/forms';
 import * as moment from 'moment';
 
 /**
@@ -15,12 +16,37 @@ import * as moment from 'moment';
   templateUrl: 'postmilk.html',
 })
 export class PostmilkPage {
+  private todo : FormGroup;
+    myDate: String = moment().format();
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor( private formBuilder: FormBuilder ) {
+    this.todo = this.formBuilder.group({
+      group:[''],
+      lh1: [false],
+      lf1: [false],
+      rh1: [false],
+      rf1: [false],
+      lh2: [false],
+      lf2: [false],
+      rh2: [false],
+      rf2: [false],
+      lh3: [false],
+      lf3: [false],
+      rh3: [false],
+      rf3: [false],
+      lh4: [false],
+      lf4: [false],
+      rh4: [false],
+      rf4: [false],
+      lh5: [false],
+      lf5: [false],
+      rh5: [false],
+      rf5: [false],
+    });
   }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad PostmilkPage');
+  logForm(){
+    console.log(this.todo.value)
+    this.todo.reset()
   }
 
 }
