d47593ac39178f49068965dc6a25ab4cc06b31a9
RobinChenRichmond <guanyu.chen@richmond.edu>
2018-03-11 14:53:41 -0400

    fix no back button problem on strip page

--- milkApp/src/pages/strip/strip.ts
+++ milkApp/src/pages/strip/strip.ts
@@ -1,29 +1,10 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {Validators, FormBuilder, FormGroup } from '@angular/forms';
+@IonicPage()
 @Component({
-  template: `
-    <form [formGroup]="todo" (ngSubmit)="logForm()">
-      <ion-item>
-        <ion-label>Stall No:</ion-label>
-        <ion-input type="text" formControlName="title"></ion-input>
-      </ion-item>
-      <ion-item>
-        <ion-label>ML</ion-label>
-        <ion-input type="text" formControlName="ml"></ion-input>
-      </ion-item>
-      <ion-item>
-        <ion-label>Balanced</ion-label>
-        <ion-checkbox formControlName="balanced" ></ion-checkbox>
-      </ion-item>
-      <ion-item>
-        <ion-label>Not Balanced</ion-label>
-        <ion-checkbox formControlName="unbalanced" ></ion-checkbox>
-      </ion-item>
-      <button ion-button type="submit" [disabled]="!todo.valid">Save</button>
-      <button ion-button type="submit" >Submit</button>
-    </form>
-  `
+  selector: 'page-strip',
+  templateUrl: 'strip.html',
 })
 export class StripPage {
   private todo : FormGroup;
