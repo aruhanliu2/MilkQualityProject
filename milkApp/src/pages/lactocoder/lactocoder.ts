a0491129f76baf8a601f231ec7562d3034b4d8e5
RobinChenRichmond <guanyu.chen@richmond.edu>
2018-04-13 15:49:53 -0400

    update lactocoder

--- milkApp/src/pages/lactocoder/lactocoder.ts
+++ milkApp/src/pages/lactocoder/lactocoder.ts
@@ -25,82 +25,62 @@ export class LactocoderPage {
   cowName1: string = ""
   milk1: string = ""
   remark1: string = ""
-  timer1: string = "Dip"
-  cowList1: number[] = []
-  DLU1: number[] = []
+  cowList1: number[] = [0,0,0,0,0]
+  DLU1: number[] = [0,0,0]
 
   cowName2: string = ""
   milk2: string = ""
   remark2: string = ""
-  timer2: string = "Dip"
-  cowList2: number[] = []
-  DLU2: number[] = []
+  cowList2: number[] = [0,0,0,0,0]
+  DLU2: number[] = [0,0,0]
 
   cowName3: string = ""
   milk3: string = ""
   remark3: string = ""
-  timer3: string = "Dip"
-  cowList3: number[] = []
-  DLU3: number[] = []
+  cowList3: number[] = [0,0,0,0,0]
+  DLU3: number[] = [0,0,0]
 
   constructor( public alerCtrl: AlertController) {
   }
 
   timer(e,param: number,timer: string){
+    let map = {"Dip":0,"Strip":1,"Wipe":2,"Attach":3,"Remove":4}
+
     if(timer!="Finished"){
       if(param==1){
-        this.cowList1.push(+moment().format("X"))
-        if(this.cowList1.length==3){
-          this.DLU1.push(this.cowList1[2]-this.cowList1[0])
-        } else if(this.cowList1.length==4){
-          this.DLU1.push(+this.cowList1[3]-this.cowList1[1])
-        } else if(this.cowList1.length==5){
-          this.DLU1.push(+this.cowList1[4]-this.cowList1[3])
+        this.cowList1[map[timer]] = +moment().format("X")
+        if(map[timer]==2){
+          this.DLU1[0] = (this.cowList1[2]-this.cowList1[0])
+        } else if(map[timer]==3){
+          this.DLU1[1] = (+this.cowList1[3]-this.cowList1[1])
+        } else if(map[timer]==4){
+          this.DLU1[2] = (+this.cowList1[4]-this.cowList1[3])
         }
         console.log(this.cowList1)
         console.log(this.DLU1)
       } else if(param==2){
-        this.cowList2.push(+moment().format("X"))
-        if(this.cowList2.length==3){
-          this.DLU2.push(this.cowList2[2]-this.cowList2[0])
-        } else if(this.cowList2.length==4){
-          this.DLU2.push(this.cowList2[3]-this.cowList2[1])
-        } else if(this.cowList2.length==5){
-          this.DLU2.push(this.cowList2[4]-this.cowList2[3])
+        this.cowList2[map[timer]] = +moment().format("X")
+        if(map[timer]==2){
+          this.DLU2[0] = (this.cowList2[2]-this.cowList2[0])
+        } else if(map[timer]==3){
+          this.DLU2[1] = (+this.cowList2[3]-this.cowList2[1])
+        } else if(map[timer]==4){
+          this.DLU2[2] = (+this.cowList2[4]-this.cowList2[3])
         }
         console.log(this.cowList2)
         console.log(this.DLU2)
       } else if(param==3){
-        this.cowList3.push(+moment().format("X"))
-        if(this.cowList3.length==3){
-          this.DLU3.push(this.cowList3[2]-this.cowList3[0])
-        } else if(this.cowList3.length==4){
-          this.DLU3.push(this.cowList3[3]-this.cowList3[1])
-        } else if(this.cowList3.length==5){
-          this.DLU3.push(this.cowList3[4]-this.cowList3[3])
+        this.cowList3[map[timer]] = +moment().format("X")
+        if(map[timer]==2){
+          this.DLU3[0] = (this.cowList3[2]-this.cowList3[0])
+        } else if(map[timer]==3){
+          this.DLU3[1] = (+this.cowList3[3]-this.cowList3[1])
+        } else if(map[timer]==4){
+          this.DLU3[2] = (+this.cowList3[4]-this.cowList3[3])
         }
         console.log(this.cowList3)
         console.log(this.DLU3)
       }
-    }
-    if(timer=="Dip"){
-      timer = "Strip"
-    } else if(timer=="Strip"){
-      timer = "Wipe"
-    } else if(timer=="Wipe"){
-
-      timer = "Attach"
-    } else if(timer=="Attach"){
-      timer = "Remove"
-    } else if(timer=="Remove"){
-      timer = "Finished"
-    }
-    if(param==1){
-      this.timer1 = timer
-    } else if(param==2){
-      this.timer2 = timer
-    } else if(param==3){
-      this.timer3 = timer
     }
   }
 
@@ -108,20 +88,17 @@ export class LactocoderPage {
     this.cowName1 = ""
     this.milk1 = ""
     this.remark1 = ""
-    this.timer1 = "Dip"
     this.cowList1 = []
 
 
     this.cowName2 = ""
     this.milk2 = ""
     this.remark2 = ""
-    this.timer2 = "Dip"
     this.cowList2 = []
 
     this.cowName3 = ""
     this.milk3 = ""
     this.remark3 = ""
-    this.timer3 = "Dip"
     this.cowList3 = []
   }
 
