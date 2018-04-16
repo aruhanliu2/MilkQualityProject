1b25aa12c799381da4f2037a12110e133022498b
RobinChenRichmond <guanyu.chen@richmond.edu>
2018-04-15 21:30:41 -0400

    optimize lactocoder functions

--- milkApp/src/pages/lactocoder/lactocoder.ts
+++ milkApp/src/pages/lactocoder/lactocoder.ts
@@ -9,16 +9,6 @@ import * as moment from 'moment';
   templateUrl: 'lactocoder.html',
 })
 export class LactocoderPage {
-  a3: string = "true"
-  a4: string = "true"
-  a5: string = "true"
-  b3: string = "true"
-  b4: string = "true"
-  b5: string = "true"
-  c3: string = "true"
-  c4: string = "true"
-  c5: string = "true"
-
   farm: string = ""
   myDate: string = moment().format()
   parlor: string = ""
@@ -31,97 +21,73 @@ export class LactocoderPage {
   prep: string = ""
   routine: string = ""
 
-
   cowName1: string = ""
   milk1: string = ""
   remark1: string = ""
-  cowList1: number[] = [0,0,0,0,0]
-  DLU1: number[] = [0,0,0]
 
   cowName2: string = ""
   milk2: string = ""
   remark2: string = ""
-  cowList2: number[] = [0,0,0,0,0]
-  DLU2: number[] = [0,0,0]
 
   cowName3: string = ""
   milk3: string = ""
   remark3: string = ""
-  cowList3: number[] = [0,0,0,0,0]
-  DLU3: number[] = [0,0,0]
+
+  cowList: number[][] = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
+  DLU: number[][] = [[0,0,0],[0,0,0],[0,0,0]]
+  buttons: boolean[][] = [[false,false,true,true,true],[false,false,true,true,true],[false,false,true,true,true]]
 
   constructor( public alerCtrl: AlertController) {
   }
 
-  timer(e,param: number,timer: string){
-    let map = {"Dip":0,"Strip":1,"Wipe":2,"Attach":3,"Remove":4}
+  buttonControl(cow: number, step: number){
+    this.cowList[cow][step] = +moment().format("X")
+    this.buttons[cow][step] = true
 
-    if(timer!="Finished"){
-      if(param==1){
-        this.cowList1[map[timer]] = +moment().format("X")
-        if(map[timer]==2){
-          this.DLU1[0] = (this.cowList1[2]-this.cowList1[0])
-        } else if(map[timer]==3){
-          if(this.cowList1[1]==0){
-            this.DLU1[1] = (+this.cowList1[3]-this.cowList1[2])
-          } else{
-            this.DLU1[1] = (+this.cowList1[3]-this.cowList1[1])
-          }
-        } else if(map[timer]==4){
-          this.DLU1[2] = (+this.cowList1[4]-this.cowList1[3])
-        }
-        console.log(this.cowList1)
-        console.log(this.DLU1)
-      } else if(param==2){
-        this.cowList2[map[timer]] = +moment().format("X")
-        if(map[timer]==2){
-          this.DLU2[0] = (this.cowList2[2]-this.cowList2[0])
-        } else if(map[timer]==3){
-          if(this.cowList2[1]==0){
-            this.DLU2[1] = (+this.cowList2[3]-this.cowList2[2])
-          } else{
-            this.DLU2[1] = (+this.cowList2[3]-this.cowList2[1])
-          }
-        } else if(map[timer]==4){
-          this.DLU2[2] = (+this.cowList2[4]-this.cowList2[3])
-        }
-        console.log(this.cowList2)
-        console.log(this.DLU2)
-      } else if(param==3){
-        this.cowList3[map[timer]] = +moment().format("X")
-        if(map[timer]==2){
-          this.DLU3[0] = (this.cowList3[2]-this.cowList3[0])
-        } else if(map[timer]==3){
-          if(this.cowList3[1]==0){
-            this.DLU3[1] = (+this.cowList3[3]-this.cowList3[2])
-          } else{
-            this.DLU3[1] = (+this.cowList3[3]-this.cowList3[1])
-          }
-        } else if(map[timer]==4){
-          this.DLU3[2] = (+this.cowList3[4]-this.cowList3[3])
-        }
-        console.log(this.cowList3)
-        console.log(this.DLU3)
+    if(step==0){
+      this.buttons[cow][2] = false
+    } else if(step==1){
+      if(this.cowList[cow][2]==0){
+        this.buttons[cow][3] = false
       }
+    } else if(step==2){
+      this.DLU[cow][0] = (this.cowList[cow][2]-this.cowList[cow][0])
+      if(this.cowList[cow][1]==0){
+        this.buttons[cow][3] = false
+      }
+    } else if(step==3){
+      if(this.cowList[cow][1]==0){
+        this.DLU[cow][1] = (+this.cowList[cow][3]-this.cowList[cow][2])
+      } else{
+        this.DLU[cow][1] = (+this.cowList[cow][3]-this.cowList[cow][1])
+      }
+      this.buttons[cow][4] = false
+    } else if(step==4){
+      this.DLU[cow][2] = (+this.cowList[cow][4]-this.cowList[cow][3])
     }
+    console.log(this.cowList[cow])
+    console.log(this.DLU[cow])
   }
 
   saveData() {
+
+    this.cowList= [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
+    this.DLU = [[0,0,0],[0,0,0],[0,0,0]]
+    this.buttons= [[false,false,true,true,true],[false,false,true,true,true],[false,false,true,true,true]]
+
     this.cowName1 = ""
     this.milk1 = ""
     this.remark1 = ""
-    this.cowList1 = []
+
 
 
     this.cowName2 = ""
     this.milk2 = ""
     this.remark2 = ""
-    this.cowList2 = []
 
     this.cowName3 = ""
     this.milk3 = ""
     this.remark3 = ""
-    this.cowList3 = []
   }
 
 }
